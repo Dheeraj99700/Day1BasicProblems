fb4e141cf3359b58930af042074cb8287c79e51f
unknown <chaaudhari.dheeraj12@gmail.com>
2022-11-01 15:53:24 +0530

    Added:Program to Print Default Values

---
+++ DefaultValues.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.Day1BasicPrograms;
+
+public class DefaultValues {
+
+	public static void main(String[] args) {
+		int val=4;
+		float vl=5;
+		System.out.println("Integer value = "+val);
+		System.out.println("Float value = "+vl);
+
+	}
+
+}
