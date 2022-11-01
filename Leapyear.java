f2159a50be132c170c28da7afe555cfe484bc01f
unknown <chaaudhari.dheeraj12@gmail.com>
2022-11-01 15:55:01 +0530

    Added:Program To check year entered is Leap year or Not

---
+++ Leapyear.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.Day1BasicPrograms;
+
+import java.util.Scanner;
+
+public class Leapyear {
+
+	public static void main(String[] args) {
+		int year;
+		 System.out.println("Enter the year");
+		 try (Scanner sc = new Scanner(System.in)) {
+			year=sc.nextInt();
+		}
+		 boolean leap = false;
+		 if (year % 4 == 0) {
+		 if (year % 100 == 0) {
+	     if (year % 400 == 0)
+		 leap = true;
+		 else
+		 leap = false;
+	     }
+		 else
+		 leap = true;
+		 }
+		 else
+		 leap = false;
+		 if (leap)
+		  System.out.println(year + " is a leap year");
+		  else
+		  System.out.println(year + " is not a leap year");
+	}
+
+}
